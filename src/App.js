78fee51ff4050cf2f95e5901343435e09bff0a29
Heojiyeon <33304871+Heojiyeon@users.noreply.github.com>
2022-09-04 23:42:10 +0900

    callback props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { nanoid } from 'nanoid';
+import React, { useState } from 'react';
+import FilterButton from './components/FilterButton';
+import Form from './components/Form';
+import Todo from './components/Todo';
 
-function App() {
+function App(props) {
+
+  const [tasks, setTasks] = useState(props.tasks);
+
+  function addTask(name) {
+    const newTask = {
+      id: `todo-${nanoid()}`,
+      name,
+      completed: false
+    };
+    setTasks([...tasks, newTask]);
+  }
+
+  const taskList = tasks.map((task) => (
+    <Todo
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id}
+    />
+  )
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+      <Form addTask={addTask} className="filters btn-group stack-exception" />
+      <FilterButton name="All" />
+      <FilterButton name="Active" />
+      <FilterButton name="Completed" />
+      <h2 id="list-heading">
+        3 tasks remaining
+      </h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+        {taskList}
+      </ul>
     </div>
   );
 }
+
 
 export default App;
