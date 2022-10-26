14e4449022979d25a1fbbad613820e7419d5e9d7
Heojiyeon <33304871+Heojiyeon@users.noreply.github.com>
2022-10-26 10:27:35 +0900

    ✨ Feat: Add Editing, filtering function

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 
 function Form(props) {
-
-    const [name, setName] = useState('');
+    const [name, setName] = useState("");
 
     function handleChange(e) {
         setName(e.target.value);
